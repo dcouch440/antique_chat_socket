053359e58547c6498b811271447f65f73414aadb
dcouch440 <dcouch440@gmail.com>
2021-05-27 12:47:03 -0700

    add knexfile

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database:process.env.DB_NAME,
+      user:process.env.PG_USER,
+      password:process.env.PG_PASS,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    }
+  },
+  test: {
+    client: 'postgresql',
+    connection: {
+      database:process.env.TEST_DB_NAME,
+      user:process.env.PG_USER,
+      password:process.env.PG_PASS,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    }
+  },
+
+};
