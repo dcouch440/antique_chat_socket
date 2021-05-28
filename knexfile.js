9e5cdc39c36ed82189f5d228628c61d5b7b49e1d
dcouch440 <dcouch440@gmail.com>
2021-05-28 14:46:30 -0700

    remove whitespace

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-
   development: {
     client: 'postgresql',
     connection: {
@@ -34,5 +33,4 @@ module.exports = {
       max: 10,
     }
   },
-
 };
