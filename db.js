ada76527d18bcfb713edf1052e9dab42dbe32a6d
dcouch440 <dcouch440@gmail.com>
2021-05-28 14:44:08 -0700

    move knex binding to another file

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 const environment = process.env.NODE_ENV || 'development';
 const knexfile = require('./knexfile');
 const knexConfig = knexfile[environment];
-const { Model } = require('objection');
 const db = require('knex')(knexConfig);
-Model.knex(db);
 module.exports = db;
